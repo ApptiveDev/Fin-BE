f3ba66844e2e5004df98932f777c5fd17d4d1e6d
zwo-n <kangjiwon@gangjiwon-ui-MacBookAir.local>
2026-03-11 23:53:49 +0900

    feat: controller patch 추가

--- src/main/java/apptive/fin/user/UserController.java
+++ src/main/java/apptive/fin/user/UserController.java
@@ -1,14 +1,30 @@
 package apptive.fin.user;
 
+import apptive.fin.auth.AuthUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/me")
-    public String getMyInfo() {
-        return "내 정보 조회";
+    public UserResponseDto getMyInfo(@AuthenticationPrincipal AuthUserDetails userDetails){
+        return userService.getMyInfo(userDetails.getUser().getId());
+    }
+
+    @PatchMapping("/me")
+    public void updateUser(
+            @RequestBody UserUpdateRequestDto request,
+            @AuthenticationPrincipal AuthUserDetails userDetails
+    ){
+        Long userId = userDetails.getUser().getId();
+        userService.updateUser(userId, request);
     }
 
 }
