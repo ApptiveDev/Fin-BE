7d032dfb563b817e90fcf998b258a7e4e4f57946
yuyeol3 <zerom50@gmail.com>
2026-03-11 14:59:56 +0900

    feat : 카카오 oauth2 로그인 추가

--- src/main/java/apptive/fin/auth/oauth/touserinfo/OAuth2UserInfoFactory.java
+++ src/main/java/apptive/fin/auth/oauth/touserinfo/OAuth2UserInfoFactory.java
@@ -9,6 +9,7 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         return switch (registrationId) {
             case "google" -> new GoogleOAuth2UserInfo(attributes);
+            case "kakao" -> new KakaoOAuth2UserInfo(attributes);
             default -> throw new BusinessException(CommonErrorCode.NOT_FOUND);
         };
     }
