b278f830fbefe126e3b23a2929fab18d73c3cc25
zwo-n <kangjiwon@gangjiwon-ui-MacBookAir.local>
2026-03-12 21:00:42 +0900

    feat: 약관 id 수정

--- src/main/java/apptive/fin/term/dto/UserTermRequestDto.java
+++ src/main/java/apptive/fin/term/dto/UserTermRequestDto.java
@@ -1,11 +1,12 @@
 package apptive.fin.term.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class UserTermRequestDto {
-
-    // FE에 약관 넘기는 용도의 Dto
+    // FE에서 동의한 약관 id 목록
     private List<Long> termIds;
 }
