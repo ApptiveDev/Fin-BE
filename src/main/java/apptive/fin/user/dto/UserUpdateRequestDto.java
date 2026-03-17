d08f5fa12bed40419b1ed8d42fb904bf6588c60c
zwo-n <kangjiwon@gangjiwon-ui-MacBookAir.local>
2026-03-17 23:01:07 +0900

    fix: dto -> record 수정

--- src/main/java/apptive/fin/user/dto/UserUpdateRequestDto.java
+++ src/main/java/apptive/fin/user/dto/UserUpdateRequestDto.java
@@ -1,12 +1,4 @@
 package apptive.fin.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class UserUpdateRequestDto {
-
-    private String email;
-    private String name;
+public record UserUpdateRequestDto (String email, String name) {
 }
