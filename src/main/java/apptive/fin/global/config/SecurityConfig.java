efef3e176d94edd9a334131f301499f4820ef565
yuyeol3 <zerom50@gmail.com>
2026-03-23 18:34:38 +0900

    fix : favicon.ico permitAll() 제외

--- src/main/java/apptive/fin/global/config/SecurityConfig.java
+++ src/main/java/apptive/fin/global/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/favicon.ico").permitAll()
+           //                     .requestMatchers("/favicon.ico").permitAll()
                                 .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
                                 .requestMatchers("/auth/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
