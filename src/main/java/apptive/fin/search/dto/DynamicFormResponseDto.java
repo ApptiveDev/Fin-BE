dbcc8c405461c5adb616865f65326ca3b67a00a0
yuyeol3 <zerom50@gmail.com>
2026-04-05 02:15:12 +0900

    fix : DynamicFormResponse ageBoundì 추가

--- src/main/java/apptive/fin/search/dto/DynamicFormResponseDto.java
+++ src/main/java/apptive/fin/search/dto/DynamicFormResponseDto.java
@@ -1,11 +1,22 @@
 package apptive.fin.search.dto;
 
+import lombok.Builder;
+
+@Builder
 public record DynamicFormResponseDto(
-    boolean showTenure,
-    int yearlyEarnDefault,
-    boolean showBankInterestRateCheckList,
+    Boolean showTenure,
+    Integer ageBound,
+    Integer yearlyEarnDefault,
+    Boolean showBankInterestRateCheckList,
     MedianIncomesDto medianIncomes
 ) {
 
+    public DynamicFormResponseDto {
+        if (showTenure == null) showTenure = true;
+        if (ageBound == null) ageBound = 34;
+        // if (yearlyEarnDefault == null);
+        if (showBankInterestRateCheckList == null) showBankInterestRateCheckList = false;
+        // if (medianIncomes == null) medianIncomes = null;
+    }
 
 }
