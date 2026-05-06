409ea0341371eb18c8b3353d690da4368aaf47e8
zwo-n <kangjiwon@gangjiwon-ui-MacBookAir.local>
2026-05-06 13:59:14 +0900

    fix: detailOption null 보호 조건 추가

--- src/main/java/apptive/fin/search/service/EligibilityFilterService.java
+++ src/main/java/apptive/fin/search/service/EligibilityFilterService.java
@@ -19,15 +19,31 @@ public class EligibilityFilterService {
     public List<Product> filterEligible(SearchRequestDto request){
         var detail = request.detailedOptions();
 
-        int age = Period.between(detail.birthdate(), LocalDate.now()).getYears();
+        // TODO : QueryDSL 도입
+        if (detail == null) return List.of();
 
-        int tenureMonths = Boolean.TRUE.equals(detail.isFirstJob())
+        int age = detail.birthdate() != null
+                ? Period.between(detail.birthdate(), LocalDate.now()).getYears()
+                : 0;
+
+        long annualIncome = detail.annualIncome() != null
+                ? detail.annualIncome()
+                : 0L;
+
+        boolean isHomeless = Boolean.TRUE.equals(detail.isHomeless());
+        boolean isHouseholder = Boolean.TRUE.equals(detail.isHouseholder());
+
+        int tenureMonths =Boolean.TRUE.equals(detail.isFirstJob())
                 ? -1
                 : (detail.tenureMonths() != null ? detail.tenureMonths() : 0);
 
+        long monthlyDeposit = detail.monthlySavingsGoal() != null
+                ? detail.monthlySavingsGoal()
+                : 0L;
+
         return productRepository.findEligibleProducts(
-                age, detail.annualIncome(), detail.isHomeless(),
-                detail.isHouseholder(),tenureMonths, detail.monthlySavingsGoal()
+                age, annualIncome, isHomeless,
+                isHouseholder,tenureMonths, monthlyDeposit
         );
 
     }
